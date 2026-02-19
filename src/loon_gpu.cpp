43f926ca39af4c0e74e6000d3502cc80d277e783
Kevin Gibson <rkevingibson@gmail.com>
2026-02-18 16:34:26 -0800

    Add additional safety check on first swapchain use. Don't think the atomic exchange is exactly necessary anymore, but doesn't hurt.

--- src/loon_gpu.cpp
+++ src/loon_gpu.cpp
@@ -2458,17 +2458,17 @@ void CommandBuffer::barrier(StageFlags                    before,
 
 
         if (t.texture == impl->m_surface.swapchain_images[impl->m_surface.current_image_idx]) {
-            auto* first_use_command
-                = &impl->m_surface
-                       .first_use_command[impl->m_surface.frame_idx % Surface::kMaxFramesInFlight];
-
-            int64_t expected = 0;
-            // We try and set the first use command value if it hasn't already been set by an
-            // earlier barrier.
-            atomic_compare_exchange(reinterpret_cast<int64_t*>(first_use_command),
-                                    &expected,
-                                    reinterpret_cast<int64_t>(buffer));
-
+            if (t.old_layout == Layout::DontCare || t.old_layout == Layout::Present) {
+                auto* first_use_command
+                    = &impl->m_surface.first_use_command[impl->m_surface.frame_idx
+                                                         % Surface::kMaxFramesInFlight];
+                int64_t expected = 0;
+                // We try and set the first use command value if it hasn't already been set by an
+                // earlier barrier.
+                atomic_compare_exchange(reinterpret_cast<int64_t*>(first_use_command),
+                                        &expected,
+                                        reinterpret_cast<int64_t>(buffer));
+            }
             if (t.new_layout == Layout::Present) {
                 impl->m_surface.transitioning_command[impl->m_surface.current_image_idx]
                     = reinterpret_cast<VkCommandBuffer>(buffer);
