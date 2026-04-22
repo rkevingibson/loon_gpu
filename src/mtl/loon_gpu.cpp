d53a3e1cc551c35e86d63f3f95ace0c782ffd932
Kevin Gibson <rkevingibson@gmail.com>
2026-04-22 10:30:58 -0700

    Tests are correctly failing gracefully, cleanup logging

--- src/mtl/loon_gpu.cpp
+++ src/mtl/loon_gpu.cpp
@@ -243,37 +243,26 @@ Device create_device(const DeviceDesc& desc) {
     auto blk = alloc.alloc(sizeof(DeviceImpl));
     if (blk.ptr == 0) { return nullptr; }
 
-    auto device_raw = MTL::CreateSystemDefaultDevice();
-    if (!device_raw) {
-        fprintf(stderr, "Failed to create metal system device.\n");
-        return nullptr;
-    }
-    fprintf(stderr, "Metal device created\n");
-    auto device = NS::TransferPtr(device_raw);
-    if (!device->supportsFamily(MTL::GPUFamilyMetal4)) {
-        fprintf(stderr, "Metal device doesn't support metal 4!\n");
-        return nullptr;
-    }
-    fprintf(stderr, "Metal device supports metal4\n");
+    auto device = NS::TransferPtr(MTL::CreateSystemDefaultDevice());
+    if (!device) { return nullptr; }
+
+    if (!device->supportsFamily(MTL::GPUFamilyMetal4)) { return nullptr; }
 
     NS::Error* error = nullptr;
 
     auto compiler_desc = make_id<MTL4::CompilerDescriptor>();
     auto compiler      = NS::TransferPtr(device->newCompiler(compiler_desc.get(), &error));
-    fprintf(stderr, "Metal Compiler created\n");
-    auto options = make_id<MTL4::CompilerTaskOptions>();
+    auto options       = make_id<MTL4::CompilerTaskOptions>();
 
     // It seems like this is a valid cast from testing.
     auto surface_metal_layer
         = NS::RetainPtr(reinterpret_cast<CA::MetalLayer*>(desc.native_window_handle));
-    fprintf(stderr, "Surface metal layer cast\n");
 
     auto residency_set_descriptor = make_id<MTL::ResidencySetDescriptor>();
     // NOTE: Not sure how much this matters, should dig in more
     residency_set_descriptor->setInitialCapacity(64);
     auto residency_set
         = NS::TransferPtr(device->newResidencySet(residency_set_descriptor.get(), nullptr));
-    fprintf(stderr, "Residency set created\n");
     // NOTE: Not 100% sure about if this is UB - I need a pointer to the device in order to capture
     // it in the lambdas, that are created during construction of the object. It probably is
     // technically undefined behviour. I'm also pretty certain it's totally fine in practice, if a
