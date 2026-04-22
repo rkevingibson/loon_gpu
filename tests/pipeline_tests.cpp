89b9423910cd3a15fb03a1cd773037f5ea0ea413
Kevin Gibson <rkevingibson@gmail.com>
2026-04-21 19:04:15 -0700

    Add basic test of shader specialization

--- tests/pipeline_tests.cpp
+++ tests/pipeline_tests.cpp
@@ -18,3 +18,52 @@ UTEST(pipeline_tests, basic_compute_compilation) {
 
     gpu::destroy_device(device);
 }
+
+struct ClearBufferArgs {
+    gpu::GpuPtr buffer;
+    uint32_t    num_elements;
+};
+
+UTEST(pipeline_tests, specialized_compute_compilation) {
+    auto device = gpu::create_device({});
+
+    const uint32_t kClearValue = 42;
+
+    auto pipeline
+        = gpu::create_compute_pipeline(device,
+                                       {
+                                           .spirv       = test::shaders::specialization,
+                                           .entry_point = "clear_buffer"_sv,
+                                       },
+                                       gpu::SpecializationConstant{
+                                           .constant_id = 0,
+                                           .int_val     = 42,
+                                           .type        = gpu::SpecializationConstantType::UInt32,
+                                       });
+    ASSERT_TRUE(pipeline);
+
+    auto                    args        = gpu::malloc(device, sizeof(ClearBufferArgs));
+    static constexpr size_t kBufferSize = 1024;
+    auto buffer      = gpu::malloc(device, kBufferSize * sizeof(uint32_t), gpu::Memory::Readback);
+    auto cpu_args    = reinterpret_cast<ClearBufferArgs*>(gpu::get_host_pointer(device, args));
+    cpu_args->buffer = buffer;
+    cpu_args->num_elements = kBufferSize;
+
+    auto queue = gpu::get_queue(device);
+
+    auto cmd = gpu::queue_start_command_recording(queue);
+
+    gpu::cmd_set_pipeline(cmd, pipeline);
+    gpu::cmd_dispatch(cmd, args, {.x = kBufferSize / 64, .y = 1, .z = 1});
+    gpu::cmd_finalize(cmd);
+
+    gpu::queue_submit(queue, cmd);
+
+    gpu::device_wait_for_idle(device);
+
+    auto cpu_buffer = reinterpret_cast<uint32_t*>(gpu::get_host_pointer(device, buffer));
+
+    for (uint32_t i = 0; i < kBufferSize; ++i) { ASSERT_EQ(cpu_buffer[i], kClearValue); }
+
+    gpu::destroy_device(device);
+}
